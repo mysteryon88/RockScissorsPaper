812bf180dea1824cbec69f6f2eac6ddba813edaa
mysteryon88 <mysteryon88@yandex.ru>
2022-09-29 01:18:18 +0300

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const Private_Key = "i will not say"
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+  	goerli: {
+  		url: `https://goerli.infura.io/v3/2b83a7c05da34d38bd6a5a98dd9cdcd4`,
+  		accounts: [`0x${Private_Key}`]
+  	}
+  }
 };
