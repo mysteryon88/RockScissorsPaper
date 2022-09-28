9b61fa8b180accc8b72728addfd5b833c7e6340e
mysteryon88 <mysteryon88@yandex.ru>
2022-09-29 01:14:47 +0300

    Revert "deploy"
    This reverts commit 9e497b7b33b15f75a3ea62075f90d03944ce5a08.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-
-const Private_Key = "i will not say"
-//ed66fc73c86dc09c2d23dca70b8e39527b69ea4aaccc17f999bdb01055fa0db6
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-  	goerli: {
-  		url: `https://goerli.infura.io/v3/2b83a7c05da34d38bd6a5a98dd9cdcd4`,
-  		accounts: [`0x${Private_Key}`]
-  	}
-  }
 };
