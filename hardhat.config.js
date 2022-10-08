c43b9459348c8ee5edf7590c2254a488e766060c
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-10-08 22:00:21 +0300

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const Private_Key = "i will not say"
+require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEY
+const endpoint = process.env.URL
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
-  	goerli: {
-  		url: `https://goerli.infura.io/v3/2b83a7c05da34d38bd6a5a98dd9cdcd4`,
-  		accounts: [`0x${Private_Key}`]
-  	}
+	goerli: {
+		url: endpoint,
+		accounts: [`0x${privateKey}`]
+	}
   }
 };
